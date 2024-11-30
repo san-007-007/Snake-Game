bdea6dea2d99c4fd92457aa43ab0e3431e33c826
Sanjeevnathan <127003228@sastra.ac.in>
2024-11-30 14:27:48 +0530

    Add files via upload

---
+++ snakegame/src/snakegame/SnakeGame.java
@@ -0,0 +1,19 @@
+package snakegame;
+
+import javax.swing.*;
+
+
+public class SnakeGame extends JFrame {
+	public SnakeGame(){
+		super("Snake Game");//SETTING TITLE
+		add(new Board());//ADDING GAME PANEL
+		pack();
+		setSize(300,300);//SETTING FRAME SIZE
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//CLOSES APPLICATION
+        setLocationRelativeTo(null);//CENTRES THE APPLICATION FRAME
+		setVisible(true);
+	}
+	public static void main(String...args){
+		SwingUtilities.invokeLater(SnakeGame::new);//INVOKING SNAKEGAME OBJECT IN MAIN METHOD
+	}
+}
